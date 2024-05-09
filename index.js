ff44f7f3b235f8fb99c6a39a804cde922f4eb2b8
ottosch <ottosch@localhost>
2024-05-09 17:08:02 -0300

    fix change postmix

--- index.js
+++ index.js
@@ -20,73 +20,50 @@ const prefixes = new Map([
 	['zpub', { bip32: { public: 0x04b24746, private: 0x04b2430c }, network: mainnet, descOpen: 'wpkh(', descClose: ')' }],
 ]);
 
+const N = BigInt('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141');
+const limit = 20;
+
 const sparrow = 'sparrow';
 const electrum = 'electrum';
 const err = 'err';
 
+let network;
+let prefix;
+
 document.querySelector('#go').addEventListener('click', () => {
 	const xpubStr = document.querySelector('#xpub').value;
 	const wif = document.querySelector('#wif').value;
-	
 	if (!xpubStr || !wif) {
 		print(err, 'Invalid input');
 		return;
 	}
 
-	const prefix = prefixes.get(xpubStr.substring(0, 4));
-	const network = {...prefix.network};
+	prefix = prefixes.get(xpubStr.substring(0, 4));
+	network = {...prefix.network};
 	network.bip32 = prefix.bip32;
 
 	const childPrivKey = ECPair.fromWIF(wif, network).privateKey;
-	const bigPrivKey = BigInt(`0x${childPrivKey.toString('hex')}`);
+	const xpub = bip32.fromBase58(xpubStr, network);
 
-	let xpub = bip32.fromBase58(xpubStr, network);
-	let recvXpub = xpub.derive(0);
-
-	let privFound;
-
-	const N = BigInt('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141');
-	const limit = 20;
-
-	// find the private key index
-	for (let index = 0; index < limit; index++) {
-		console.log(`index: ${index}`);
-		let bufIndex = Buffer.alloc(4);
-		bufIndex.writeInt32BE(index);
-
-		let data = Buffer.concat([recvXpub.publicKey, bufIndex]);
-		let hmac = crypto.createHmac('sha512', recvXpub.chainCode).update(data).digest();
-
-		let left = hmac.subarray(0, 32);
-		let bigLeft = BigInt(`0x${left.toString('hex')}`);
-		
-		let calcKey = (bigPrivKey - bigLeft) % N;
-		if (calcKey < 0n) {
-			calcKey += N;
-		}
-
-		let calcHex = calcKey.toString(16).padStart(64, '0');
-		let calcBuffer = Buffer.from(calcHex, 'hex');
-
-		let parentPrivKey = ECPair.fromPrivateKey(calcBuffer, { network: network });
-		let pubkey = parentPrivKey.publicKey;
-		if (pubkey.equals(recvXpub.publicKey)) {
-			console.log(`found`);
-			privFound = parentPrivKey;
-			break;
-		}
+	let childIndex = 0;
+	let parentPrivKey = findParentPrivKey(xpub, childPrivKey, childIndex);
+	if (!parentPrivKey) {
+		childIndex = 1;
+		parentPrivKey = findParentPrivKey(xpub, childPrivKey, childIndex);
 	}
 
-	if (!privFound) {
+	if (!parentPrivKey) {
 		console.log('not found');
 		print(err, 'Data does not match. Either this private key is not part of this xpub or it\'s too deep');
 		return;
 	}
 
-	const xprv = bip32.fromPrivateKey(privFound.privateKey, recvXpub.chainCode, network);
+	const xprv = bip32.fromPrivateKey(parentPrivKey.privateKey, xpub.derive(childIndex).chainCode, network);
 	const bigParentPrivKey = BigInt(`0x${xprv.privateKey.toString('hex')}`);
 
-	let data = Buffer.concat([xpub.publicKey, Buffer.alloc(4)]);
+	const bufIndex = Buffer.alloc(4);
+	bufIndex.writeUInt32BE(childIndex);
+	let data = Buffer.concat([xpub.publicKey, bufIndex]);
 	let hmac = crypto.createHmac('sha512', xpub.chainCode).update(data).digest();
 	let left = hmac.subarray(0, 32);
 	let bigLeft = BigInt(`0x${left.toString('hex')}`);
@@ -111,6 +88,41 @@ document.querySelector('#go').addEventListener('click', () => {
 	console.log(output);
 	print(sparrow, output);
 });
+
+const findParentPrivKey = (xpub, childPrivkey, childIndex) => {
+	const childXpub = xpub.derive(childIndex);
+	const bigPrivKey = BigInt(`0x${childPrivkey.toString('hex')}`);
+
+	// find the private key index
+	for (let index = 0; index < limit; index++) {
+		console.log(`index: ${index}`);
+		let bufIndex = Buffer.alloc(4);
+		bufIndex.writeInt32BE(index);
+
+		let data = Buffer.concat([childXpub.publicKey, bufIndex]);
+		let hmac = crypto.createHmac('sha512', childXpub.chainCode).update(data).digest();
+
+		let left = hmac.subarray(0, 32);
+		let bigLeft = BigInt(`0x${left.toString('hex')}`);
+
+		let calcKey = (bigPrivKey - bigLeft) % N;
+		if (calcKey < 0n) {
+			calcKey += N;
+		}
+
+		let calcHex = calcKey.toString(16).padStart(64, '0');
+		let calcBuffer = Buffer.from(calcHex, 'hex');
+
+		let parentPrivKey = ECPair.fromPrivateKey(calcBuffer, { network: network });
+		let pubkey = parentPrivKey.publicKey;
+		if (pubkey.equals(childXpub.publicKey)) {
+			console.log(`found`);
+			return parentPrivKey;
+		}
+
+		return null;
+	}
+};
 
 document.querySelector('#clear').addEventListener('click', () => {
 	document.querySelector('#xpub').value = '';
